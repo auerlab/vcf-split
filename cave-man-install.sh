ce62b8dbb06bbbd5f53c4cb790586e211233fe40
Jason Bacon <bacon@barracuda.uits.uwm.edu>
2021-07-12 19:00:18 -0500

    cave-man-install.sh: Script for dev/testing builds

---
+++ cave-man-install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+##########################################################################
+#
+#   Perform a cave-man install for development and testing purposes.
+#   For production use, this software should be installed via a package
+#   manager such as Debian packages, FreeBSD ports, MacPorts, pkgsrc, etc.
+#       
+#   History:
+#   Date        Name        Modification
+#   2021-07-12  Jason Bacon Begin
+##########################################################################
+
+# Default to ../local if LOCALBASE is not set
+: ${LOCALBASE:=../local}
+
+# OS-dependent tricks
+# Set rpath to avoid picking up libs installed by package managers in
+# /usr/local/lib, etc.
+case $(uname) in
+*)
+    LIBDIR=$(realpath $LOCALBASE/lib)
+    export LDFLAGS="-L$LIBDIR -Wl,-rpath,$LIBDIR:/usr/lib:/lib"
+    ;;
+
+esac
+
+make clean install
+ldd vcf-split
