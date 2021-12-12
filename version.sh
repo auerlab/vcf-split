19713a4e0f739840335ced2e170b822398688146
Jason Bacon <bacon@moray.acadix.biz>
2021-12-12 13:25:34 -0600

    Import improved version.sh from ad2vcf

--- version.sh
+++ version.sh
@@ -1,15 +1,11 @@
 #!/bin/sh -e
 
 if [ -e .git ]; then
-    v=$(git describe --tags | cut -d - -f 1-2)
-    commit=${v#*-}
-    version=${v%-*}
-elif [ -n "$PORTVERSION" ]; then
-    version=$PORTVERSION
-elif [ -n "$PKGVERSION" ]; then
-    version=$PKGVERSION
+    version=$(git describe --tags | cut -d - -f 1-2 | tr - .)
+elif [ -n "$VERSION" ]; then
+    version=$VERSION
 else
-    version=Unknown
+    version="Unknown-version"
 fi
 echo $version
 
