429b108035cab83a9d53d40a665d5cc35adaffe4
Jason Bacon <bacon@moray.acadix.biz>
2022-02-08 08:26:00 -0600

    version.sh: Note need to set VERSION in ports env

--- version.sh
+++ version.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
+##########################################################################
+#   Be sure to set VERSION in the build phase of any ports, since there
+#   will be no .git in the work directory.
+##########################################################################
+
 if [ -e .git ]; then
     version=$(git describe --tags | cut -d - -f 1-2 | tr - .)
 elif [ -n "$VERSION" ]; then
